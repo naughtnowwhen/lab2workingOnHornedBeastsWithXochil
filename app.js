8cebcdc506a6fa44ee88239900cff8dceb54dc64
Peter <naughtnowwhen@gmail.com>
2018-11-24 00:29:03 -0800

    got it to display a pic

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let hornedBeasts = [];
 
 
 
+
 function readJson () {
   $.get('page-1.json', 'json')
     .then(data => {
@@ -27,6 +28,7 @@ function readJson () {
 $(() => readJson());
 
 
+
 // description: "A unicorn and a narwhal nuzzling their horns"
 // horns: 1
 // image_url: "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
@@ -35,13 +37,18 @@ $(() => readJson());
 
 
 let HornMaker = function(catchall){
+
+
   this.image_url = catchall.image_url;
+  this.image_urlForDom = `<img src="${catchall.image_url}" alt="">`;
   this.title = catchall.title;
   this.description = catchall.description;
   this.horns = catchall.horns;
   this.keyword = catchall.keyword;
   hornedBeasts.push(this);
 };
+
+arrofClones = [];
 
 HornMaker.prototype.render = function(){
   $('main').append('<div class="clone"></div>');
@@ -51,8 +58,21 @@ HornMaker.prototype.render = function(){
   $clone.find('h1').text(this.title);
   $clone.find('p').text(this.description);
   $clone.find('img').attr('src', this.image_url);
+  let fullClone = $clone.html();
+  var htmlobject =
+  // let jsonVersion = JSON.stringify(fullClone);
+  // console.log(fullClone);
   $clone.removeClass('clone');
   $clone.attr('class', this.title);
+  let $main = $('main');
+
+};
+
+console.log(hornedBeasts.length);
+
+HornMaker.prototype.passUrl = function () {
+
+
 };
 
 // let trial = new HornMaker('description',4,'image_url','keyword','title');
@@ -72,23 +92,26 @@ HornMaker.prototype.render = function(){
 
 
 // $('select[name="icecream"]').on('change', function() {
-  
+
 //   let $selection = $(this).val();
 //   console.log($selection);
 //   $('img').hide()
-  // $(`img[data-flavor="${$selection}"]`).show()
+// $(`img[data-flavor="${$selection}"]`).show()
 // })
+
+let $main = $('main');
+console.log($main.children());
 
 $('select[name="horned"]').on('change',function(){
   let $selection = $(this).val();
   let $playAround = $(this);
   console.log($playAround);
   console.log($selection);
-  $('img').hide();
-  
+  // $('img').hide();
+
   // $(`img[keyword="${$selection}"]`).show();
 
-  let url = "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-….jpg/wholesale-halloween-costume-prop-unicorn.jpg";
+  let url = 'https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-….jpg/wholesale-halloween-costume-prop-unicorn.jpg';
 
   url.show();
 
@@ -105,10 +128,10 @@ $('select[name="horned"]').on('change',function(){
   }
   // $(`img[description="${$selection}"]`).show();
 
-  
+
   // $(`img[description: "A unicorn and a narwhal nuzzling their horns"]`).show();
 
-  // hornedBeasts[0].image_url.show();  
+  // hornedBeasts[0].image_url.show();
 
 
 
@@ -131,7 +154,49 @@ let imgAsker = function (){
 
 imgAsker();
 
+function imgappender (hornedBeastIndexToDisplay){
+  let hideAllImgs = $('img').hide();
+  let description = hornedBeasts[hornedBeastIndexToDisplay].description;
+  console.log(description);
+  let listForDisplay = $('ul[id="listForDisplay"]');
+  let image_url = hornedBeasts[hornedBeastIndexToDisplay].image_url;
+  let image_alt = hornedBeasts[hornedBeastIndexToDisplay].keyword;
 
+  let makeForAppend = `<img src="${image_url}" alt="${image_alt}">`;
+  listForDisplay.append(makeForAppend);
+
+  // image_url =
+  // hornedBeasts[hornedBeastIndexToDisplay].image_url = mainImgDisplay.html();
+
+}
+
+
+$("#forCLickingNewImgs").click(function(){
+let index = $('#forGivingAnIndex').val();
+imgappender(index);
+});
+
+function btnNewImgClick () {
+  let btn = $('#forCLickingNewImgs');
+  btn.on('click', function(){
+    console.log('click');
+  }
+  );
+}
+
+//only seems to work in dev tools.
+// imgappender();
+
+
+
+
+$(document).ready(function() {
+//why is hornedbeasts inaccesbile
+//neither console log work
+  console.log(hornedBeasts[0]);
+  console.log(hornedBeasts.length);
+
+});
 
 // let tryIt = $('select');
 // console.log(tryIt.html);
